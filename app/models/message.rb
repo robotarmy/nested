965568733711890b25977db56746d8805a28c8fd
Curtis Schofield <github@robotarmyma.de>
2011-02-08 12:28:17 -0800

    test 10 pass; did need identity_attributes for nested attributes for

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
   belongs_to :identity
+  accepts_nested_attributes_for :identity
 end
--- spec/integration/message_spec.rb
+++ spec/integration/message_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe Message do
   context "takes my public key to make a message" do
-    let(:message_size) do
-      Message.count
-    end
-    before do
+    let(:post_message) do
       visit new_message_path
       fill_in 'message[body]' , :with => 'Hello there unknown - what time is it there?'
-      fill_in 'message[identity][public_key]' , :with => 'd34db33f'
+      fill_in 'message[identity_attributes][public_key]' , :with => 'd34db33f'
       click_button 'send'
+      Message.last
     end
 
     it "creates a message" do
-      message_size.should < Message.count
+      lambda {
+        post_message
+      }.should change(Message,:count).by(1)
     end
 
     it "creates an identity"
