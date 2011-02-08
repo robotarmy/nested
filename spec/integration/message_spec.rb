e487244c5504cc0030ee1b851832a7ae47f76203
Curtis Schofield <github@robotarmyma.de>
2011-02-08 10:50:40 -0800

    test 9 failing

--- spec/integration/message_spec.rb
+++ spec/integration/message_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe Message do
-  it "takes my public key to make a message" do
-    visit new_message_path
-    fill_in 'message[body]' , :with => 'Hello there unknown - what time is it there?'
-    fill_in 'message[identity][public_key]' , :with => 'd34db33f'
-    click_button 'send'
+  context "takes my public key to make a message" do
+    let(:message_size) do
+      Message.count
+    end
+    before do
+      visit new_message_path
+      fill_in 'message[body]' , :with => 'Hello there unknown - what time is it there?'
+      fill_in 'message[identity][public_key]' , :with => 'd34db33f'
+      click_button 'send'
+    end
+
+    it "creates a message" do
+      message_size.should < Message.count
+    end
+
+    it "creates an identity"
+    it "creates a message with an identity" 
   end
 end
