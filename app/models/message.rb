2274a9f675d616ab9f712fd1f295ec2e801d8a63
Curtis Schofield <github@robotarmyma.de>
2011-02-08 16:33:32 -0800

    test 13-14 -

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :identity
   accepts_nested_attributes_for :identity
+  def identity_attributes=(attributes)
+    self.identity = Identity.find_or_create_by_public_key(attributes[:public_key])
+  end
 end
--- spec/integration/message_spec.rb
+++ spec/integration/message_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe Message do
   context "takes my public key to make a message" do
-    let(:post_message) do
+    PUBLIC_KEY=%%d34db33f%
+
+    def post_message
       visit new_message_path
       fill_in 'message[body]' , :with => 'Hello there unknown - what time is it there?'
-      fill_in 'message[identity_attributes][public_key]' , :with => 'd34db33f'
+      fill_in 'message[identity_attributes][public_key]' , :with => PUBLIC_KEY
       click_button 'send'
-      Message.last
     end
 
     it "creates a message" do
@@ -16,7 +17,21 @@ describe Message do
       }.should change(Message,:count).by(1)
     end
 
-    it "creates an identity"
-    it "creates a message with an identity" 
+    it "creates an identity" do
+      lambda {
+        post_message
+      }.should change(Identity,:count).by(1)
+
+      lambda {
+        post_message
+      }.should_not change(Identity,:count).by(1)
+    end
+
+    it "creates a message with an identity" do
+      post_message
+      message = Message.last
+      message.identity.should_not be_nil
+      message.identity.should == Identity.find_by_public_key(PUBLIC_KEY)
+    end
   end
 end
